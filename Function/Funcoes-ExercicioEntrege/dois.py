7309475292a2f97a7eebc962f31914c87276fa1b
Nicoly <nicoly.oavelino@gmail.com>
2022-05-26 08:07:51 -0300

    up

---
+++ Function/Funcoes-ExercicioEntrege/dois.py
@@ -0,0 +1,22 @@
+# 2.
+# Considere a função (consulte as demais funções no código-fonte apresentado em aula):
+
+# def posicaoVazia(tabela, linha,coluna):
+#     i = indiceLista(linha,coluna)
+        
+#     if tabela[i] == "-":
+#         return True
+#     else:
+#         return False
+
+# E considere que tabela = ['-','X','O','-','O','X','-','-','-'], linha=0 e coluna =0. Assinale a alternativa com o valor que será retornado pela função posicaoVazia:
+
+def posicaoVazia(tabela, linha,coluna):
+  i = indiceLista(linha,coluna)
+  if tabela[i] == "-":
+    return True
+  else:
+    return False
+tabela = ['-','X','O','-','O','X','-','-','-']
+posicaoVazia(tabela,0,0)
+# True
